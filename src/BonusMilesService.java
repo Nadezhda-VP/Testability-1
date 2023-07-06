629164ee230ab9407f11f2e32623285d597f2d25
Nady <nadezhda.vp.qa@gmail.com>
2023-07-06 19:54:46 +0300

    Main changed

---
+++ src/BonusMilesService.java
@@ -0,0 +1,8 @@
+public class BonusMilesService {
+    public int calculate(int price) {
+        int bonus = 20;
+        int result;
+        result = price / bonus;
+        return result;
+    }
+}
